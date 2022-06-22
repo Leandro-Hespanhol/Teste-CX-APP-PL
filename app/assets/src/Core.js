a38b22748febbc3dcf59e0a872e36a41cf255b47
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-06-22 11:44:13 -0300

    input, button and click event

--- app/assets/src/Core.js
+++ app/assets/src/Core.js
@@ -1,5 +1,31 @@
 const aNewFunction = () => {
   // A content here
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.className = 'update-ticket-cep';
+
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.innerText = 'Atualizar Ticket'
+
+  let mainContent = document.getElementById('main-content');
+  
+  const updateButton = document.getElementById('botao-atualizar')
+  updateButton.addEventListener('click', async () => {
+    const selectInput = document.getElementsByClassName('update-ticket-cep');
+    const inputText = selectInput.value;
+    if ((inputText !== 8) || (inputText.split('-').join('') !== 8)) {
+      return alert('Cep precisa conter exatamente 8 números, com ou sem traço')
+    }
+    return fetch(`https://viacep.com.br/ws/${inputText}/json/`)
+      .then((data) => data)
+  })
+
+  mainContent.appendChild(input);
+  mainContent.appendChild(button)
+  
+  return mainContent
 };
 
 const Core = {
